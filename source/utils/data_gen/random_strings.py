4251659d1f11618b3083198c118ffb4f6ba5ec05
h-pgy <hpougy@gmail.com>
2025-08-04 10:58:53 -0300

    hotfix documentos com zero

--- source/utils/data_gen/random_strings.py
+++ source/utils/data_gen/random_strings.py
@@ -7,9 +7,9 @@ def random_int_string(lengths: list[int], separator: str = '.', final_separator:
     if final_separator is not None:
         final_length = lengths[-1]
         lengths = lengths[:-1]
-        generated_string = separator.join(str(random.randint(0, length*10-1)).zfill(length) for length in lengths)
-        generated_string += final_separator + str(random.randint(0, final_length*10-1)).zfill(final_length)
+        generated_string = separator.join(str(random.randint(1, length*10-1)).zfill(length) for length in lengths)
+        generated_string += final_separator + str(random.randint(1, final_length*10-1)).zfill(final_length)
     else:
-        generated_string = separator.join(str(random.randint(0, length*10-1)).zfill(length) for length in lengths)
+        generated_string = separator.join(str(random.randint(1, length*10-1)).zfill(length) for length in lengths)
 
     return generated_string
